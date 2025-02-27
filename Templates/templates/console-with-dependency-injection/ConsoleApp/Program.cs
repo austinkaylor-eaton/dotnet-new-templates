2a8c66bd396cd76df99321f4db29b8c6615aaf13
Austin Kaylor <austinjkaylor@eaton.com>
2025-02-26 22:26:04 -0500

    Structuring the Console.App the way I want to

--- Templates/templates/console-with-dependency-injection/ConsoleApp/Program.cs
+++ Templates/templates/console-with-dependency-injection/ConsoleApp/Program.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.Extensions.Hosting;
 
+// Create the default Application Host Builder
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
 // TODO: Add your services to the container.
 
+// Build the Application Host Builder into an Application Host
 using IHost host = builder.Build();
+
+// TODO: Add your work here.
+await Console.Out.WriteLineAsync("Hello World with C#");
+
+// Run the Application Host
 await host.RunAsync();
+
+
